2a25cdf28adcb985df8149ecc8caba2fc747fcdc
sandhya <svuksept2020@gmail.com>
2024-12-05 14:17:46 +0000

    feat: implement record shop methods

--- src/main/java/com/northcoders/recordshopapi/service/RecordShopServiceImpl.java
+++ src/main/java/com/northcoders/recordshopapi/service/RecordShopServiceImpl.java
@@ -1,43 +1,37 @@
 package com.northcoders.recordshopapi.service;
 
 import com.northcoders.recordshopapi.model.Album;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RecordShopServiceImpl implements RecordShopService{
     @Override
     public Album createAlbum(Album album) {
         return null;
     }
-
     @Override
     public Album updateAlbum(Album album) {
         return null;
     }
-
     @Override
     public void deleteAlbum(Album album) {
-
     }
-
     @Override
     public Album getAlbumById(Long id) {
         return null;
     }
-
     @Override
     public Album getAlbumByGenre(String genre) {
         return null;
     }
-
     @Override
     public Album getAlbumByArtistName(Long artistId) {
         return null;
     }
-
     @Override
     public Album getAlbumByReleasedDate(Long artistId) {
         return null;
     }
-
     @Override
     public Album getAlbumByArtistId(Long artistId) {
         return null;
